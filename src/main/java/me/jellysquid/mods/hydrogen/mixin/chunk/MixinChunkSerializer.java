0a8240f731272369c2f3b06ea7b88e1ef1b699fd
Mario0051 <windowsmario64@gmail.com>
2021-09-10 01:20:06 +0000

    Update MixinChunkSerializer.java

--- src/main/java/me/jellysquid/mods/hydrogen/mixin/chunk/MixinChunkSerializer.java
+++ src/main/java/me/jellysquid/mods/hydrogen/mixin/chunk/MixinChunkSerializer.java
@@ -2,7 +2,7 @@ package me.jellysquid.mods.hydrogen.mixin.chunk;
 
 import net.minecraft.block.Block;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureManager;
 import net.minecraft.util.math.ChunkPos;
@@ -27,15 +27,15 @@ import java.util.function.Consumer;
 
 @Mixin(ChunkSerializer.class)
 public abstract class MixinChunkSerializer {
-    private static final ThreadLocal<NbtCompound> CAPTURED_TAGS = new ThreadLocal<>();
+    private static final ThreadLocal<CompoundTag> CAPTURED_TAGS = new ThreadLocal<>();
 
     @Shadow
-    private static void loadEntities(ServerWorld world, NbtCompound nbt, WorldChunk chunk) {
+    private static void writeEntities(CompoundTag nbt, WorldChunk chunk) {
         throw new UnsupportedOperationException();
     }
 
     @Inject(method = "deserialize", at = @At("HEAD"))
-    private static void captureTag(ServerWorld world, StructureManager structureManager, PointOfInterestStorage poiStorage, ChunkPos pos, NbtCompound tag, CallbackInfoReturnable<ProtoChunk> cir) {
+    private static void captureTag(ServerWorld world, StructureManager structureManager, PointOfInterestStorage poiStorage, ChunkPos pos, CompoundTag tag, CallbackInfoReturnable<ProtoChunk> cir) {
         // We can't access the method parameters in the later redirect, so capture them for this thread
         CAPTURED_TAGS.set(tag);
     }
@@ -45,29 +45,29 @@ public abstract class MixinChunkSerializer {
                                      TickScheduler<Block> blockTickScheduler, TickScheduler<Fluid> fluidTickScheduler,
                                      long inhabitedTime, @Nullable ChunkSection[] sections,
                                      @Nullable Consumer<WorldChunk> loadToWorldConsumer) {
-        NbtCompound rootTag = CAPTURED_TAGS.get();
+        CompoundTag rootTag = CAPTURED_TAGS.get();
 
         if (rootTag == null) {
             throw new IllegalStateException("No captured tag was found");
         }
 
-        NbtCompound level = rootTag.getCompound("Level");
+        CompoundTag level = rootTag.getCompound("Level");
 
         // The (misleadingly named) writeEntities function below only cares about these two tags
         // However, the lambda can end up staying loaded with the chunk if it isn't within ticking radius of a player yet
         // In order to prevent huge NBT blobs from remaining loaded in memory all the time, we can strip all the other
         // data to save a fair bit of memory.
-        NbtCompound strippedTag = new NbtCompound();
+        CompoundTag strippedTag = new CompoundTag();
         strippedTag.put("Entities", level.getList("Entities", 10));
         strippedTag.put("TileEntities", level.getList("TileEntities", 10));
 
         return new WorldChunk(world, pos, biomes, upgradeData, blockTickScheduler, fluidTickScheduler, inhabitedTime, sections, (chunk) -> {
-            loadEntities((ServerWorld) world, strippedTag, chunk);
+            writeEntities(strippedTag, chunk);
         });
     }
 
     @Inject(method = "deserialize", at = @At("RETURN"))
-    private static void releaseTag(ServerWorld world, StructureManager structureManager, PointOfInterestStorage poiStorage, ChunkPos pos, NbtCompound tag, CallbackInfoReturnable<ProtoChunk> cir) {
+    private static void releaseTag(ServerWorld world, StructureManager structureManager, PointOfInterestStorage poiStorage, ChunkPos pos, CompoundTag tag, CallbackInfoReturnable<ProtoChunk> cir) {
         // Avoid leaking tags in memory
         CAPTURED_TAGS.remove();
     }
